d823b1c0c118d8afe01202783c05fb231bb1d3bf
Matias Hernandez <matias.hernandez@a-dedo.cl>
2014-02-27 12:46:06 -0300

    fix web.js

--- web.js
+++ web.js
@@ -1,10 +1,6 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express.createServer();
+var app = express();
 
-app.get('/',function(request,response){
-	response.send_file(__dirname + '/dist/index.html');
-}).configure(function(){
-	app.use('/images',express.static(__dirname+'/dist/images'));
-	app.use('/styles',express.static(__dirname+'/dist/styles'));
-}).listen(port);
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 3000);
