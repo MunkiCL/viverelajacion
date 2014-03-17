e6024464280449f2a96a84f605ddfa525353ae78
Matias Hernandez <matias.hernandez@a-dedo.cl>
2014-03-16 22:48:45 -0300

    set to use 1.9.3

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,15 +1,15 @@
 class Producto < BaseModel
-    has_attached_file :imagen, :styles => { :small => "240x240>", :large => "300x300>" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :imagen, :content_type => /\Aimage\/.*\Z/
-    validates_attachment_presence :imagen
+  has_attached_file :imagen, :styles => { :small => "240x240>", :large => "300x300>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :imagen, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :imagen
 
 
-    def self.by_type(clase)
-        self.all(:conditions => ["clase = ?", clase])
-    end
+  def self.by_type(clase)
+    self.where('clase = ?',clase)
+  end
 
 
-    def self.promociones
-        where('descuento >= 0').by_creation
-    end
+  def self.promociones
+    where('descuento >= 0').by_creation
+  end
 end
