2fc028bebc404e77c721ee18a17fd6325d82553b
Matias Hernandez <matias.hernandez@a-dedo.cl>
2014-03-28 17:06:49 -0300

    #11075266508730 fixed

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -3,6 +3,22 @@ class Producto < BaseModel
   validates_attachment_content_type :imagen, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :imagen
 
+  after_create    :post_to_facebook
+
+  def post_to_facebook
+    if ENV['FACEBOOK_ACCESS_TOKEN'] and self.descuento > 0
+        owner = FbGraph::User.me(ENV['FACEBOOK_ACCESS_TOKEN'])
+        pages = owner.accounts
+        page  = pages.detect {|page| page.identifier == ENV['FACEBOOK_PAGE_ID']}
+        page.feed!(
+            :message => "Nueva Promocion!! #{self.nombre}"
+            :link => "http://www.viverelajacion.cl/store/",
+            :name => "Nueva Promocion!! #{self.nombre}",
+            :description => truncate(raw(self.descripcion),:length => 150,:omision => '...',:escape => false),
+            :picture => self.imagen.url(:small)
+        )
+    end
+  end
 
   def self.by_type(clase)
     self.where('clase = ?',clase)
@@ -10,6 +26,6 @@ class Producto < BaseModel
 
 
   def self.promociones
-    where('descuento >= 0').by_creation
+    where('descuento > 0').by_creation
   end
 end
